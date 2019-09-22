d2cdffde915febc4f3f3bea4af10ccbbf1e94797
yAmAdA0150 <omnis9999@gmail.com>
2019-09-22 16:22:42 +0900

    editing users_controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email, :keyword)
+    params.require(:user).permit(:name, :email)
   end
 end
