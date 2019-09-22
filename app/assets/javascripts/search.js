60b96b6788cd43bdcaeb0efc09937e16ad03b74c
yAmAdA0150 <omnis9999@gmail.com>
2019-09-22 13:38:20 +0900

    delete consolo.logs

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -3,6 +3,7 @@ $(document).on('turbolinks:load', function() {
   var search_list = $("#user-search-result");
   
   function appendUser(user) {
+
     var html1 =  ` <div class="chat-group-user clearfix">
                     <p class="chat-group-user__name">${user.name}</p>
                     <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</a>
@@ -19,27 +20,29 @@ $(document).on('turbolinks:load', function() {
     }
 
     $('#user-search-result').on("click", ".chat-group-user__btn--add", function(e) {
-      console.log(document);
-      console.log('add');
+
       $(this).parent().remove();
       var target = e.target;
       var username = $(target).attr('data-user-name');
       var userid = $(target).attr('data-user-id');
+
       var html2 = ` <div class="chat-group-user clearfix js-chat-member" id="chat-group-user-${userid}">
                       <input name="group[user_ids][]" type="hidden" value="${userid}">
                       <p class="chat-group-user__name">${username}</p>
                       <a class="user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn">削除</a>
                     </div>`;
+
       $("#chat-group-users").append(html2);
 
     });
     $('.chat-group-form__field--right').on("click", ".chat-group-user__btn--remove", function() {
-      console.log('remove');
       $(this).parent().remove();
     });
 
     $("#user-search-field").on("keyup", function() {
+
       var input = $("#user-search-field").val();
+      
       $.ajax({
         type: 'GET',
         url: '/users',
